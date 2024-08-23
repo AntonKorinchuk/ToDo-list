73f984fa06b9669a024f61cb7bf1dd3cf31c6913
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-08-23 09:04:49 +0300

    fixed urls

--- todo/urls.py
+++ todo/urls.py
@@ -13,15 +13,51 @@ from todo.views import (
 )
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="index"),
-    path("create/", TaskCreateView.as_view(), name="task-create"),
-    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("tags/", TagsListView.as_view(), name="tag-list"),
-    path("tags/create/", TagsCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update/", TagsUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete/", TagsDeleteView.as_view(), name="tag-delete"),
-    path("<int:pk>/toggle/", toggle_complate_task, name="task-complete"),
+    path(
+        "",
+        HomePageView.as_view(),
+        name="index"
+    ),
+    path(
+        "task/create/",
+        TaskCreateView.as_view(),
+        name="task-create"
+    ),
+    path(
+        "task/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+    path(
+        "task/<int:pk>/complate/",
+        toggle_complate_task,
+        name="task-complete"
+    ),
+    path(
+        "tags/",
+        TagsListView.as_view(),
+        name="tag-list"
+    ),
+    path(
+        "tags/create/",
+        TagsCreateView.as_view(),
+        name="tag-create"
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagsUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagsDeleteView.as_view(),
+        name="tag-delete"
+    ),
 ]
 
 app_name = "todo"
